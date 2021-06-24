a46a4b74a7482230665a54cf3fa0513f0d44fa46
blueC4P <osamag4g@gmail.com>
2021-06-24 14:23:40 +0100

    redux store config

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -4,17 +4,29 @@ import { Link } from "react-router-dom";
 export const LoginScreen = () => {
   return (
     <>
-      <h3 className="auth__title">LoginScreen</h3>
+      <h3 className="auth__title">Login</h3>
 
       <form>
-        <input type="text" placeholder="email" name="email" className="auth__input" autoComplete="off"/>
-        <input type="text" placeholder="password" name="password" className="auth__input"/>
-        <button 
-        type="submit" 
-        className="btn btn-primary btn-block"
-        >Login</button>
+        <input
+          type="text"
+          placeholder="email"
+          name="email"
+          className="auth__input"
+          autoComplete="off"
+        />
+        <input
+          type="text"
+          placeholder="password"
+          name="password"
+          className="auth__input"
+        />
+        <button type="submit" className="btn btn-primary btn-block">
+          Login
+        </button>
+
         <div className="auth__social-networks">
           <p>Login with social networks</p>
+
           <div className="google-btn">
             <div className="google-icon-wrapper">
               <img
@@ -28,10 +40,8 @@ export const LoginScreen = () => {
             </p>
           </div>
         </div>
-        <Link 
-        className="link"
-        to="/auth/register">
-            Create new acount
+        <Link className="link" to="/auth/register">
+          Create new acount
         </Link>
       </form>
     </>
